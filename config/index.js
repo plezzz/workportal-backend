a51f46169c6e93053515f35092e545b67fd30e0e
plezzz <nikid12e@gmail.com>
2020-12-08 10:12:07 +0200

    test heroku

--- config/index.js
+++ config/index.js
@@ -13,6 +13,18 @@ const config = {
         saltRounds: 11,
         expire: '1h',
         pricePrecision: 100
+    },
+    production: {
+        port: process.env.PORT || 3000,
+        dbURL: url || 'mongodb://localhost:27017/WorkPortal',
+        origin: ['http://localhost:5555', 'http://localhost:4200'],
+        template: 'hbs',
+        publicDir: 'public',
+        cookie: 'x-auth-token',
+        secret: 'SuperSecretSecret',
+        saltRounds: 11,
+        expire: '1h',
+        pricePrecision: 100
     }
 };
 module.exports = config[env];
