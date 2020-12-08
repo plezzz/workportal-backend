5072ef24d7c29c9dca3049f63467b584f919980f
plezzz <nikid12e@gmail.com>
2020-12-08 10:16:59 +0200

    test heroku

--- config/index.js
+++ config/index.js
@@ -1,20 +1,9 @@
 const url = require('./secretURL');
 const env = process.env.NODE_ENV || 'development';
-
-const config = {
-    development: {
-        port: process.env.PORT || 3000,
-        dbURL: url || 'mongodb://localhost:27017/WorkPortal',
-        origin: ['http://localhost:5555', 'http://localhost:4200'],
-        template: 'hbs',
-        publicDir: 'public',
-        cookie: 'x-auth-token',
-        secret: 'SuperSecretSecret',
-        saltRounds: 11,
-        expire: '1h',
-        pricePrecision: 100
-    },
-    production: {
+console.log(env)
+console.log(process.env)
+config =
+     {
         port: process.env.PORT || 3000,
         dbURL: url || 'mongodb://localhost:27017/WorkPortal',
         origin: ['http://localhost:5555', 'http://localhost:4200'],
@@ -26,5 +15,5 @@ const config = {
         expire: '1h',
         pricePrecision: 100
     }
-};
-module.exports = config[env];
+
+module.exports = config;
